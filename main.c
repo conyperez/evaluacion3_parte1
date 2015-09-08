f88dafb3f93a71c7e6d0c782636e2f7c5ff12b0f
Constanza Pérez Merry <conny.caamila@gmail.com>
2015-09-07 23:14:35 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#ifndef STDIO_H
+#include <stdio.h>
+#define STDIO_H
+#endif
+
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
+
+#ifndef GRAFO_H
+#include "grafo.h"
+#define GRAFO_H
+#endif
+
+int main(){
+
+  return 0;
+}
