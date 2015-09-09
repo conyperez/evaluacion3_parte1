5587c5d11e6bc62f96973a4abe423dcb436aabc2
Constanza Pérez Merry <conny.caamila@gmail.com>
2015-09-09 15:52:47 -0300

    Update cola.h

--- cola.h
+++ cola.h
@@ -13,20 +13,18 @@
 #define STRING_H
 #endif
 
-#define CAPACIDAD 9
-
 typedef struct _cola{
 	int capacidad;
 	int ultimo;
 	char *elementos;
 }Cola;
 
-Cola *creaCola(){
+Cola *creaCola(int unaCapacidad){
 	Cola *tmp;
 	tmp = malloc(sizeof(Cola));
-	tmp->capacidad = CAPACIDAD;
+	tmp->capacidad = unaCapacidad;
 	tmp->ultimo = -1;
-	//tmp->elementos = malloc(tmp->capacidad*sizeof(char));
+	tmp->elementos = malloc(tmp->capacidad*sizeof(char));
 	return tmp;
 }
 
@@ -36,5 +34,48 @@ int esVaciaCola(Cola *cola){
 	}else{
 		return 0;
 	}
+}
+
+int encolar(Cola *cola, char unaUbicacion){
+    if(cola->ultimo+1 == cola->capacidad){
+        return -1;
+    }
+    cola->ultimo++;
+    cola->elementos[cola->ultimo] = unaUbicacion;
+    return 1;
+}
+
+int decolar(Cola *cola){
+    if(esVaciaCola(cola)){
+        return -1;
+    }
+    char ubicacion = cola->elementos[0];
+    int i;
+    for(i=0; i<cola->ultimo; i++){
+        cola->elementos[i] = cola->elementos[i+1];
+    }
+    cola->ultimo--;
+    return ubicacion;
+}
+
+void vaciaCola(Cola *cola){
+    while(!esVaciaCola(cola)){
+        decolar(cola);
+    }
+}
+
+void eliminaCola(Cola *cola){
+    free(cola->elementos);
+    free(cola);
+}
+
+void recorrerCola(Cola *cola){
+    printf("Inicio Cola\n");
+    printf("Tamaño cola: %d\n", cola->ultimo+1);
+    int i;
+    for(i=0; i<=cola->ultimo; i++){
+        printf("Lista[%d] = %d\n", i, cola->elementos[i]);
+    }
+    printf("Fin Cola\n");
 }
 
