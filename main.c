b20b1b7f680acde706e8acaa117a61eaa60c02a5
Constanza Pérez Merry <conny.caamila@gmail.com>
2015-09-13 00:39:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -13,11 +13,28 @@
 #define GRAFO_H
 #endif
 
+#define tam 9
+
 typedef Nodo Grafo;
 
 int main(){
+  
   Grafo grafo[tam];
   inicializaVecinos(grafo);
+
+  agregaArista(grafo, 0, 1);
+  agregaArista(grafo, 0, 2);
+  agregaArista(grafo, 1, 2);
+  agregaArista(grafo, 1, 4);
+  agregaArista(grafo, 3, 4);
+  agregaArista(grafo, 2, 7);
+  agregaArista(grafo, 3, 5);
+  agregaArista(grafo, 4, 7);
+  agregaArista(grafo, 5, 6);
+  agregaArista(grafo, 1, 5);
+  agregaArista(grafo, 6, 7);
+  agregaArista(grafo, 5, 7);
+  agregaArista(grafo, 7, 8);
 
   return 0;
 }
